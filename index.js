a3e7d6ac8dad9530c8b1719b2a5669b7340c4fe3
Alesandro Silva <silva_alesandro@outlook.com>
2022-06-08 11:30:49 -0400

    cria o index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const { Telegraf } = require('telegraf')
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
