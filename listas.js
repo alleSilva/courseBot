dc2bca09284147f042f282ba58e2612b7255dee3
Alesandro Silva <silva_alesandro@outlook.com>
2022-06-10 02:41:26 -0400

    cria as principais acoes por categoria de curso

---
+++ listas.js
@@ -0,0 +1,56 @@
+const categorias = [
+  'Data science & afins',
+  'Desenvolvimento Web',
+  'Games',
+  'Hacking',
+  'Mobile',
+  'Ui/Ux Design'
+]
+
+const web = [
+  {name: 'Front End', url: 'Front'},
+  {name: 'Back End', url: 'Back'}
+]
+
+const front = [
+  {name: 'HTML & CSS para iniciantes - Origamid', url: 'https://t.me/+0zUoP8jruGg0MGIx'},
+  {name: 'Curso Completo de Bootstrap 4 + jQuery com 6 Projetos reais', url: 'https://t.me/+tblpaue7Is4wMGEx'},
+  {name: 'Bootstrap 5 Do Basico ao Avancado com 4 projetos - Matheus Battisi', url: 'https://t.me/+Jh5uLcENBWA5OWUx'},
+  {name: 'FrontPush - Nasser Yousef Ali', url: 'https://t.me/+ACQWzfpgz5RiYzBh'}
+]
+
+const design = [
+  {name: 'Ui Design Para Iniciantes - Origamid', url: 'https://t.me/+e11gizjfnKNjNGYx'},
+  {name: 'Ui Design Avançado - Origamid', url: 'https://t.me/+K8CHjH3g2SRjZDkx'},
+  {name: 'Curso Tipografia Avançada - Origamid', url: 'https://t.me/+44uIcMyewAQ1OTVh'},
+  {name: 'Ux Design Heurísticas - Origamid', url: 'https://t.me/+CLNrd7hblrhjODU5'},
+  {name: 'Curso Adobe XD - Origamid', url: 'https://t.me/+p2T_2xSu0VU2MzRh'},
+  
+]
+
+const back = [
+  {name: 'PHP MySQL Estudonauta 2019', url: 'https://t.me/+ACVyCTN7NdxlMWNh'},
+  {name: 'PHP para Iniciantes - Gustavo Guanabara (Estudonauta) 2018', url: 'https://t.me/+TSC2xPZoe9BhZTJh'},
+  {name: 'Laravel Developer - UpInside', url: 'https://t.me/+_iQpaGXGwJU1YTIx'},
+  {name: 'Rocketseat - Ignite - Trilha NodeJs', url: 'https://t.me/+SzqQb4-KJmBkMDgx'}
+]
+
+const dados = [
+  {name: 'Formação Cientista de Dados - DSA', url: 'https://t.me/+eR1DqQGn2ewyZDBh'},
+  {name: 'Engenheiro Machine Learning - DSA', url: 'https://t.me/+q4d6aHPM5IlkZDIx'},
+  {name: 'Engenharia de Dados - DSA', url: 'https://t.me/+aQYmofw3-_c3OGRh'}
+]
+
+const games = [
+  {name: 'Aprendendo a Criar Games com C++ e a Unreal Engine 4', url: 'https://t.me/+TAZOVv47rzw4NWVh'},
+]
+
+const hacking = [
+  {name: 'Solyd Pentest Profissional', url: 'https://t.me/+9dzJClJTW2hlZjZh'},
+]
+
+const mobile = [
+  {name: 'React Native Desenvolva APPs Android iOS - COD3R', url: 'https://t.me/+NyMXp7EPIVE1ZTEx'},
+]
+
+module.exports = {front, design, back, dados, games, hacking, categorias, web, mobile}
