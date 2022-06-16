3dd6f6cfe0a791da7724cbd380f4ad0547c7d4f5
Alesandro Silva <silva_alesandro@outlook.com>
2022-06-16 02:37:53 -0400

    lista cursos e encaminha para o canal das aulas

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Telegraf, Markup } = require('telegraf')
 const {session} = require('telegraf')
 const db = require('./db') //selectCategorias, selectSubCategoria, selectCursos
-const {catObj, categorias, subcategoria,cursos, categorias_subs} = require('./listas')
+const {catObj, subcategoria,cursos, categorias_subs} = require('./listas')
 
 const token = process.env.BOT_TOKEN
 if (token === undefined) {
@@ -16,83 +16,95 @@ bot.command('start', async (ctx, next) => {
   ctx.session = {
     categorias: [],
     sub: [],
+    sub2: [],
     cursos: [],
     current_cat: 0,
-    current_sub: 0
+    current_sub: 0,
+    current_sub_name: ''
   }
 
-  categorias.then(res => ctx.session.categorias = res)
+  const categorias = await db.selectCategorias()
+  ctx.session.categorias = categorias
+
+ /*  let cat = ctx.session.categorias
+  for(item in cat) {
+    const sub2 = await db.selectSubCategoria(cat[item].id)
+    ctx.session.sub2[item] = catObj(cat[item].id, cat[item].categoria, sub2)
+  } */
+  //categorias.then(res => ctx.session.categorias = res)
   //subcategoria.then(res => console.log(res))
   //cursos.then(res => console.log(res))
   return await ctx.reply('<b>Boas Vindas!</b>', {
     parse_mode: 'HTML',
     ...Markup.inlineKeyboard([
       Markup.button.url('🤑 Fazer doação', 'https://t.me/+JK7wnzLtPA1jZTVh'),
-      Markup.button.callback('▶️ Continuar', 'continuar')
+      Markup.button.callback('▶️ Continuar', 'categorias')
     ])
   })
 })
 //-1001505347688
 //-1001691603480
 
-/*   bot.action('continuar2', (ctx, next) => {
-    lista.then(lst => {
-      console.log(lst)
-      return ctx.reply('<b>Escolha uma categoria:\n</b>', {
-      parse_mode: 'HTML',
-      ...Markup.inlineKeyboard(lst.map(item => Markup.button.callback(item.nome, 'encaminha')), {columns: 2})
-    })})
-  }) */
-
-  bot.action('encaminha', (ctx) => {
-    lista.then(lst =>  ctx.telegram.forwardMessage(ctx.chat.id, -1001691603480, lst[0].mid))
-    })
-
-bot.action('continuar', async (ctx) => {
+bot.action('categorias', async (ctx) => {
+  //const categorias = await db.select
   ctx.session.sub = ctx.session.categorias.map(async item => {
     const sub = db.selectSubCategoria(item.id)
     return await sub.then(r => catObj(item.id, item.categoria, r))
-  })
+  }) 
+
+ /*  ctx.session.categorias.map(async item => {
+    
+    return await sub.then(r => catObj(item.id, item.categoria, r))
+  }) */
+
+  /* ctx.session.sub = ctx.session.categorias.map(async item => {
+    const sub = await db.selectSubCategoria(item.id)
+    return await sub.map(r => catObj(item.id, item.categoria, r))
+  }) */
   return await ctx.reply('<b>Escolha uma categoria:\n</b>', {
     parse_mode: 'HTML',
-    ...Markup.inlineKeyboard(ctx.session.categorias.map(item => Markup.button.callback(item.categoria, `cat ${item.id}`)), {columns: 2})
+    ...Markup.inlineKeyboard(ctx.session.categorias.map(item => Markup.button.callback(item.categoria, `sub ${item.id}`)), {columns: 2})
   })
    }
 )
-//Markup.button.callback(item.categoria, `sub ${r.id}`)
-bot.action(/cat (.+)/, async (ctx) => {
+
+bot.action(/sub (.+)/, async (ctx) => {
   const option = ctx.match[1]
   ctx.session.current_cat = option
   return await ctx.session.sub.map(r => r.then(x => {if (x.id == option) {
     ctx.reply(`<b>Cursos ${x.categoria}:</b>`, {parse_mode: 'HTML', 
-  ...Markup.inlineKeyboard(x.subs.map(item => Markup.button.callback(item.subcategoria, `sub ${item.id}`)), {columns: 2})})
+  ...Markup.inlineKeyboard(x.subs.map(item => Markup.button.callback(item.subcategoria, `cursos ${item.subcategoria} ${item.id}`)), {columns: 2})})
   }}))
 })
 
-bot.action(/sub (.+)/, async (ctx) => {
-  const option = ctx.match[1]
+bot.action(/cursos (.+)/, async (ctx) => {
+  let sub_info = ""
+  let option = ""
+  const infos = ctx.match[0].split(" ")
+  if (infos.length == 3) {
+    option = infos[2]
+    sub_info = infos[1]
+  } else {
+    option = infos[infos.length -1] 
+    for(let i = 1; i < infos.length - 1; i++) {
+      sub_info = sub_info + " " + infos[i]
+    }
+  }
+
   ctx.session.current_sub = option
+  //console.log(ctx.session.sub.map(async r => await r.then(x => x)))
   const cursos = await db.selectCursos(ctx.session.current_cat, ctx.session.current_sub)
-  //await cursos.then(res => ctx.session.cursos = res)
+  
   ctx.session.cursos = cursos
-  console.log(ctx.session.cursos)
-    
-  ctx.session.sub.map(r => r.then(x => {if (x.id == option) {
-    ctx.reply(`<b>Cursos ${x.categoria}:</b>`, {parse_mode: 'HTML', 
-  ...Markup.inlineKeyboard(x.subs.map(item => Markup.button.callback(item.subcategoria, `cursos ${item.categoria} ${item.id}`)), {columns: 2})})
-  }}))
-})
-
-bot.action(/cursos (.+)/, (ctx) => {
-  const option = ctx.match[1]
-  console.log(ctx.match)
-  console.log('>>>', ctx.session)
-    
-  ctx.session.cursos.map(x => {
-    ctx.reply(`<b>Cursos ${x.nome}:</b>`, {parse_mode: 'HTML', 
-  ...Markup.inlineKeyboard(x.cursos.map(item => Markup.button.callback(item.nome, `curso ${item.mid}`)), {columns: 2})})
+ 
+  ctx.reply(`<b>Cursos ${sub_info}:</b>`, {parse_mode: 'HTML', 
+  ...Markup.inlineKeyboard(cursos.map(item => Markup.button.callback(item.nome, `encaminha ${item.mid}`)), {columns: 2})})
   
 })
+
+bot.action(/encaminha (.+)/, (ctx) => {
+  const mid = ctx.match[1]
+  ctx.telegram.forwardMessage(ctx.chat.id, -1001691603480, mid)
 })
 
 bot.launch()
